a7c005da49281b1c77ad5e970eae842c8fa98366
Francielle Dellamora Menegotto <francidellamora@gmail.com>
2022-01-26 00:39:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const canvas = document.
+    querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+
+class Projectile {
+    constructor(x,y,radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const players = new Player(x,y, 30, 'blue')
+players.draw()
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach(Projectile =>{
+        Projectile.update()
+    })
+}
+
+
+addEventListener('click', (event) => {
+    
+})
+
+animate()
